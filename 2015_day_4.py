81a36113a90a81759f00120cd9c9f47816e3191d
Corey Wang <corefire2004@gmail.com>
2022-04-05 14:54:00 -0400

    done

---
+++ 2015_day_4.py
@@ -0,0 +1,15 @@
+import hashlib
+input = "ckczppom"
+counter=0
+p1=True
+while True:
+    counter+=1
+    c=str(counter)
+    hash=input+c
+    result = hashlib.md5(hash.encode()).hexdigest()
+    if result[0:5]=="00000" and p1:
+        print(f'part 1: {counter}')
+        p1=False
+    if result[0:6]=="000000":
+        print(f'part 2: {counter}')
+        exit()
