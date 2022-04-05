e682fd4cf9a4ae92dbe59a799b75b1576390aa9a
Corey Wang <corefire2004@gmail.com>
2022-04-05 13:55:55 -0400

    p2

--- 2015_day_2.py
+++ 2015_day_2.py
@@ -999,13 +999,17 @@ input="""20x3x11
 21x2x22
 14x12x8"""
 p1=0
+p2=0
 input=input.split("\n")
 for i in input:
     il=[int(a) for a in i.split("x")]
     min=sorted(il)[0]
-    print(min)
     l,w,h=il[0],il[1],il[2]
+    ribbon=2*(l+w+h-max([l,w,h]))
+    bow=l*w*h
+    p2=p2+ribbon+bow
     dl=[2*l*w,2*l*h,2*w*h]
     dl.sort()
     p1=p1+sum(dl)+dl[0]//2
 print(p1)
+print(p2)
